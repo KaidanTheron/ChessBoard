a52de6823e3a58dac1e6f96fb87739b53c8cce3a
Merveille van Eck <merveillevaneck@gmail.com>
2024-08-21 22:50:16 +0200

    got location sub working for emulators

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -4,38 +4,46 @@ import { SafeAreaView, Text, View } from "react-native";
 import * as Location from "expo-location";
 
 export default function Page() {
-    const [location, setLocation] = useState<null | Location.LocationObject>(null);
-    const [permission, setPermission] = useState(false);
-    const [locSub, setLocSub] = useState<null | Location.LocationSubscription>(null);
+  const [location, setLocation] = useState<null | Location.LocationObject>(
+    null
+  );
+  const [permission, setPermission] = useState(false);
+  const [locSub, setLocSub] = useState<null | Location.LocationSubscription>(
+    null
+  );
 
-    useEffect(() => {
+  useEffect(() => {
+    (async () => {
+      const status = await Location.requestForegroundPermissionsAsync();
+      if (!status) return;
 
-        (async () => {
-
-            const status = await Location.requestForegroundPermissionsAsync();
-            if (!status) return;
-
-            const locSub = await Location.watchPositionAsync({ accuracy: Location.Accuracy.Lowest }, (location) => {
-                setLocation(location)
-            })
-
-            setLocSub(locSub);
-        })()
-
-        return () => {
-            if (!!locSub) locSub.remove();
+      const locSub = await Location.watchPositionAsync(
+        { accuracy: Location.Accuracy.High, timeInterval: 300 },
+        (location) => {
+          if (!!location) setLocation(location);
         }
+      );
 
-    }, [])
+      setLocSub(locSub);
+    })();
 
-    return (
-        <SafeAreaView className="flex flex-row justify-between">
-            <View className="bg-red-400">
-                <Text>{!!location ? `${location.coords.latitude} : ${location.coords.longitude}` : "No location yet"}</Text>
-            </View>
-            <Link href="/" className="p-2 bg-slate-300">
-                Go back nour
-            </Link>
-        </SafeAreaView>
-    )
+    return () => {
+      if (!!locSub) locSub.remove();
+    };
+  }, []);
+
+  return (
+    <SafeAreaView className="flex flex-row justify-between">
+      <View className="bg-red-400">
+        <Text>
+          {!!location
+            ? `${location?.coords?.latitude} : ${location?.coords?.longitude}`
+            : "No location yet"}
+        </Text>
+      </View>
+      <Link href="/" className="p-2 bg-slate-300">
+        Go back nour
+      </Link>
+    </SafeAreaView>
+  );
 }
